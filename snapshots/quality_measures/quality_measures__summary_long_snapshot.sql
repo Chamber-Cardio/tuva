4788919d261ed38d097f95c6104deae776bbc1bd
Sarah Morgan <sarah.mcdill.morgan+github@gmail.com>
2026-04-01 16:22:43 -0600

    modify unqiue key for quality_measures__summary_long_snapshot

--- snapshots/quality_measures/quality_measures__summary_long_snapshot.sql
+++ snapshots/quality_measures/quality_measures__summary_long_snapshot.sql
@@ -11,14 +11,12 @@
     , "tags": "quality_measures"
     , "strategy": "check"
     , "check_cols": ["denominator_flag", "numerator_flag", "exclusion_flag", "performance_flag", "evidence_date", "evidence_value", "exclusion_date", "exclusion_reason"]
-    , "unique_key": "unique_key": "person_id||coalesce(measure_id, '')||coalesce(measure_name, '')||coalesce(measure_version, '')||coalesce(cast(performance_period_begin as varchar), '')||coalesce(cast(performance_period_end as varchar), '')"
+    , "unique_key": "person_id||coalesce(measure_id, '')||coalesce(measure_name, '')||coalesce(measure_version, '')||coalesce(cast(performance_period_begin as varchar), '')||coalesce(cast(performance_period_end as varchar), '')"
     , "enabled": var('snapshots_enabled',False) == true and var('quality_measures_enabled',var('claims_enabled',var('clinical_enabled',var('tuva_marts_enabled',False)))) == true | as_bool
     , "hard_deletes": "invalidate"
   })
 }}
 
 select * from {{ ref('quality_measures__summary_long') }}
-"unique_key": "person_id||coalesce(measure_id, '')||coalesce(measure_name, '')||coalesce(measure_version, '')||coalesce(cast(performance_period_begin as varchar), '')||coalesce(cast(performance_period_end as varchar), '')"
-
 
 {% endsnapshot %}
